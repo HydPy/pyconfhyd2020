b570fc1f54e140ce25bd5531da9cee4d6dc76aa0
Bharat Saraswat <bsaraswat45@gmail.com>
2020-10-04 12:58:25 +0530

    Update deploy.sh
    updating GH_TOKEN variable name

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ commit_changes() {
 }
 
 push_files_to_github() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/HydPy/hydpy.github.io.git
+  git remote add origin-pages https://GH_TOKEN@github.com/HydPy/hydpy.github.io.git
   git push origin-pages master
 }
 
